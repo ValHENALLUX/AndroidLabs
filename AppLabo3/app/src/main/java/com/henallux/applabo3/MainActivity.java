2723b5bd41e0810c0150136898165f44c527d08e
Valentin <valentin.veny.01@student.henallux.be>
2017-10-19 20:04:22 +0200

    Labo3 - Fonctionnel

--- AppLabo3/app/src/main/java/com/henallux/applabo3/MainActivity.java
+++ AppLabo3/app/src/main/java/com/henallux/applabo3/MainActivity.java
@@ -4,6 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,9 +16,37 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public boolean onCreateOptionMenu(Menu menu)
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
     {
-        this.getMenuInflater().inflate(R.menu.menu_main, menu);
+        MenuInflater mi = getMenuInflater();
+        mi.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings)
+        { return true;}
+
+        switch(item.getItemId())
+        {
+            case R.id.itemCallId :
+            {
+                Toast.makeText(getBaseContext(), "make a phone call", Toast.LENGTH_LONG).show();
+            }
+            case R.id.itemEmailId :
+            {
+                Toast.makeText(getBaseContext(), "create a new mail", Toast.LENGTH_LONG).show();
+            }
+            case R.id.itemSearchId :
+            {
+                Toast.makeText(getBaseContext(), "search the web", Toast.LENGTH_LONG).show();
+            }
+        }
         return true;
     }
 }
